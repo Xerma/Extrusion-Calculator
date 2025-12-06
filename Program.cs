83657aae4dbdd558ec29f72011378b8a9a135512
cheave2 <cheave2@wgu.edu>
2025-12-05 22:39:44 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Extrusion_Calculator
+{
+    public class Program()
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type help to see all commands");
+        }
+    }
+}
