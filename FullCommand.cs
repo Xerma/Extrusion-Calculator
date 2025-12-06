29f4e5a09dce80c9238989d4faee9098371b7fa9
cheave2 <cheave2@wgu.edu>
2025-12-06 00:20:03 -0600

    Data type to store command information

---
+++ FullCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extrusion_Calculator
+{
+    public struct FullCommand(string c, double s)
+    {
+        public string Command { get; set; } = c;
+        public double Size { get; set; } = s;
+    }
+}
