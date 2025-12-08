314bdc0fa3b290b96ada7da6e6c8dfa0c6356cbd
cheave2 <cheave2@wgu.edu>
2025-12-08 00:20:14 -0600

    Renamed fields and updated types

--- InventoryManager.cs
+++ InventoryManager.cs
@@ -2,37 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Extrusion_Calculator
 {
     public class InventoryManager
     {
-        private static readonly string _appDataRoot = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        private static readonly string _appFolder = Path.Combine(_appDataRoot, "Extrusion Calculator");
-        private static readonly string _appInventory = Path.Combine(_appFolder, "Inventory.json");
+        private static readonly string AppDataRoot = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private static readonly string AppFolder = Path.Combine(AppDataRoot, "Extrusion Calculator");
+        private static readonly string AppInventory = Path.Combine(AppFolder, "Inventory.json");
+
+        private static SortedSet<double> InventoryData;
 
         public static void SetupInventory()
         {
             if (!DoesAppFolderExist())
             {
-                Directory.CreateDirectory(_appFolder);
+                Directory.CreateDirectory(AppFolder);
             }
 
             if (!DoesAppInventoryFileExist())
             {
-                File.WriteAllText(_appInventory, "[]");
+                File.WriteAllText(AppInventory, "[]");
             }
         }
 
         private static bool DoesAppFolderExist()
         {
-            return Directory.Exists(_appFolder);
+            return Directory.Exists(AppFolder);
         }
 
         private static bool DoesAppInventoryFileExist()
         {
-            return File.Exists(_appInventory);
+            return File.Exists(AppInventory);
+        }
+
+        public static SortedSet<double> LoadInventory()
+        {
+            if (DoesAppInventoryFileExist())
+            {
+                string json = File.ReadAllText(AppInventory);
+                InventoryData = JsonSerializer.Deserialize<SortedSet<double>>(json)!;
+                return InventoryData;
+            }
+            else
+            {
+                InventoryData = new SortedSet<double>();
+                return InventoryData;
+            }
+        }
+
+        public static void SaveInventory(SortedSet<double> i)
+        {
+            string json = JsonSerializer.Serialize(i, new JsonSerializerOptions() { WriteIndented = true });
+            File.WriteAllText(AppInventory, json);
         }
     }
 }
