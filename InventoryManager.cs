da7e0e6df612e7500457aaa184815a99c07eea72
cheave2 <cheave2@wgu.edu>
2025-12-07 20:29:36 -0600

    Added InventoryManager to check for and build folder and inventory file

---
+++ InventoryManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extrusion_Calculator
+{
+    public class InventoryManager
+    {
+        private static readonly string _appDataRoot = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private static readonly string _appFolder = Path.Combine(_appDataRoot, "Extrusion Calculator");
+        private static readonly string _appInventory = Path.Combine(_appFolder, "Inventory.json");
+
+        public static void SetupInventory()
+        {
+            if (!DoesAppFolderExist())
+            {
+                Directory.CreateDirectory(_appFolder);
+            }
+
+            if (!DoesAppInventoryFileExist())
+            {
+                File.WriteAllText(_appInventory, "[]");
+            }
+        }
+
+        private static bool DoesAppFolderExist()
+        {
+            return Directory.Exists(_appFolder);
+        }
+
+        private static bool DoesAppInventoryFileExist()
+        {
+            return File.Exists(_appInventory);
+        }
+    }
+}
