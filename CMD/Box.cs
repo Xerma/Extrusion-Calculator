34a54744de3d2dfff49887610491c596c0024def
cheave2 <cheave2@wgu.edu>
2025-12-08 00:14:44 -0600

    Quick foundations for box command

---
+++ CMD/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extrusion_Calculator.CMD
+{
+    public class Box
+    {
+        public static void BoxCommand(double[] d, SortedSet<double> inv)
+        {
+            double boxWidth = d[0];
+            double boxHeight = d[1];
+            string widthPieces = "";
+            string heightPieces = "";
+
+            // start with last item (largest in sorted set)
+            // if remainder, try next size, repeat
+            // if remainder at the end, custom size needed
+        }
+    }
+}
