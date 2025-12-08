123434e9c6e35a3d5ef0d0e2814b8edde942ac96
cheave2 <cheave2@wgu.edu>
2025-12-07 20:29:02 -0600

    Updated ParseCommand with more detailed checks. Added additional commands to switch in RunCommand

--- CommandManager.cs
+++ CommandManager.cs
@@ -10,6 +10,10 @@ namespace Extrusion_Calculator
 {
     public class CommandManager
     {
+        private static readonly List<string> CommandList = [ "help", "h", "add", "a", "delete", "d", "inv", "i", "clear", "c" ];
+        private static readonly List<string> CommandSizeList = [ "add", "a", "delete", "d", "box", "b" ];
+        private static readonly List<string> CommandNoSizeList = ["help", "h", "inv", "i", "clear", "c"];
+
         public static void RunCommand(string command)
         {
             FullCommand fullCommandParsed = ParseCommand(command);
@@ -20,6 +24,29 @@ namespace Extrusion_Calculator
             {
                 case "help":
                     Help.HelpCommand();
+                    break;
+                case "h":
+                    Help.HelpCommand();
+                    break;
+                case "add":
+                    break;
+                case "a":
+                    break;
+                case "delete":
+                    break;
+                case "d":
+                    break;
+                case "inv":
+                    break;
+                case "i":
+                    break;
+                case "clear":
+                    break;
+                case "c":
+                    break;
+                case "box":
+                    break;
+                case "b":
                     break;
                 default:
                     Console.Write("");
@@ -33,34 +60,46 @@ namespace Extrusion_Calculator
             string? command = "";
             double size = 0;
 
-            try
-            {
-                inputSplit = input.Split(' ');
-            }
-            catch
-            {
-                Console.Write("Parsing Error: Command and size must be separated by a single space");
-            }
+            try { inputSplit = input.Split(' '); }
+            catch { Console.Write("Parsing Error: Command and size must be separated by a single space"); }
 
-            try
-            {
-                command = inputSplit[0].ToLower();
-            }
-            catch
-            {
-                Console.WriteLine("Command Error: Ensure the command only contains text");
-            }
+            try { command = inputSplit[0].ToLower(); }
+            catch { Console.WriteLine("Command Error: Ensure the command only contains text"); }
 
-            // need to check if command is in list of commands to avoid incorrect error messages (help triggered Size Error
-            try
+            if (inputSplit.Length > 1 && CommandNoSizeList.Contains(command))
             {
-                size = Convert.ToDouble(inputSplit[1]);
+                try
+                {
+                    size = Convert.ToDouble(inputSplit[1]);
+                }
+                catch
+                {
+                    Console.WriteLine($"Command Error: No size needed for command '{command}'");
+                }
             }
-            catch
+            else if (inputSplit.Length == 1 && CommandSizeList.Contains(command))
             {
-                Console.WriteLine("Size Error: Ensure the size only contains numbers");
+                try
+                {
+                    size = Convert.ToDouble(inputSplit[1]);
+                }
+                catch
+                {
+                    Console.WriteLine($"Command Error: Command '{command}' needs a size");
+                }
             }
-
+            else if (inputSplit.Length == 2 && CommandSizeList.Contains(command))
+            {
+                try
+                {
+                    size = Convert.ToDouble(inputSplit[1]);
+                }
+                catch
+                {
+                    Console.WriteLine("Size Error: Ensure the size only contains numbers");
+                }
+            }
+            
             FullCommand r = new(command, size);
             return r;
         }
