9618b90eb14dd19f3a4d104a12b40e94e3b90d51
cheave2 <cheave2@wgu.edu>
2025-12-08 00:19:17 -0600

    Added helper method MainWrite to be used alongside clear command

--- Program.cs
+++ Program.cs
@@ -6,14 +6,18 @@ namespace Extrusion_Calculator
     public class Program()
     {
         private static string? _input;
+        public static SortedSet<double>? InventoryData;
 
         static void Main()
         {
             InventoryManager.SetupInventory();
+            InventoryData = InventoryManager.LoadInventory();
 
-            Console.WriteLine("Type help to see all commands");
-            Console.WriteLine("Return nothing to exit");
-
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                InventoryManager.SaveInventory(InventoryData);
+            };
+            MainWrite();
             MainLoop();
         }
 
@@ -22,8 +26,14 @@ namespace Extrusion_Calculator
             do
             {
                 _input = Console.ReadLine();
-                CommandManager.RunCommand(_input);
+                CommandManager.RunCommand(_input, InventoryData);
             } while (!String.IsNullOrWhiteSpace(_input));
+        }
+
+        public static void MainWrite()
+        {
+            Console.WriteLine("Type help to see all commands");
+            Console.WriteLine("Return nothing to exit");
         }
     }
 }
