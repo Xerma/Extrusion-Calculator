32b796e4d1943342a7be7f650eb29025e8571d9b
cheave2 <cheave2@wgu.edu>
2025-12-08 00:17:52 -0600

    Added an array version of the struct for the box command

---
+++ DoubleCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extrusion_Calculator
+{
+    public struct DoubleCommand(string c, double d)
+    {
+        public string Command { get; set; } = c;
+        public double Size { get; set; } = d;
+    }
+
+    public struct DoubleArrayCommand(string c, double[] d)
+    {
+        public string Command { get; set; } = c;
+        public double[] Dims { get; set; } = d;
+    }
+}
