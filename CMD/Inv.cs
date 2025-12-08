989a45ce10aa18e789c1d011f072c1cf263f2349
cheave2 <cheave2@wgu.edu>
2025-12-08 00:15:38 -0600

    Updated inventory to SortedSet Added item formatting when printing

---
+++ CMD/Inv.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extrusion_Calculator.CMD
+{
+    public class Inv
+    {
+        public static void InvCommand(SortedSet<double> inv)
+        {
+            Console.WriteLine("Inventory: " + string.Join(", ", inv.Select(x => $"{x}\"")));
+        }
+    }
+}
