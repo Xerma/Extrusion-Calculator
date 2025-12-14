0cf6c8d701146117833cbd09e68dadf1aafe1a5b
cheave2 <cheave2@wgu.edu>
2025-12-14 10:49:30 -0600

    Renamed to generic Structs

---
+++ Structs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extrusion_Calculator
+{
+    public struct ConsoleCommand(string c, string[] a)
+    {
+        public string Command { get; set; } = c;
+        public string[] Args { get; set; } = a;
+    }
+}
