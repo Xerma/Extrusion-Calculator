276e8088ec69a870f5c081f1bca51c6f0399152c
cheave2 <cheave2@wgu.edu>
2025-12-14 22:22:12 -0600

    Created Add command

---
+++ CMD/Add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extrusion_Calculator.CMD
+{
+    public class Add
+    {
+        public static void AddCommand(InventoryPiece invPiece, SortedSet<InventoryPiece> invList)
+        {
+            invList.Add(new InventoryPiece(invPiece.Length, invPiece.End1, invPiece.End2));
+        }
+    }
+}
