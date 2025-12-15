2f735bf83f5db38cd8dbd2ec4e0614d563b9f3af
cheave2 <cheave2@wgu.edu>
2025-12-14 22:24:41 -0600

    Added Delete command

---
+++ CMD/Delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extrusion_Calculator.CMD
+{
+    public class Delete
+    {
+        public static void DeleteCommand(InventoryPiece invPiece, SortedSet<InventoryPiece> invList)
+        {
+            if (invList.Contains(invPiece))
+            {
+                invList.Remove(invPiece);
+            }
+        }
+    }
+}
