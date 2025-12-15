4491c265080c59598e04bf7d299e9b0a7b1dc62f
cheave2 <cheave2@wgu.edu>
2025-12-14 22:25:05 -0600

    Simplified Help command

--- CMD/Help.cs
+++ CMD/Help.cs
@@ -8,33 +8,36 @@ namespace Extrusion_Calculator.CMD
 {
     public class Help
     {
-        private const string ThickLine = "==================================================";
-        private const string ThinLine = "--------------------------------------------------";
-
         public static void HelpCommand()
         {
-            Console.WriteLine(ThickLine);
+            ConsoleHelper.WriteThickLine();
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("AVAILABLE COMMANDS:");
-            Console.WriteLine(ThickLine);
+            ConsoleHelper.WriteThinLine();
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("'box' or 'b'");
             Console.WriteLine("Calculate what pieces are needed to form a box");
             Console.WriteLine("Example: box 48 x 96");
-            Console.WriteLine(ThinLine);
+            ConsoleHelper.WriteThinLine();
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("'inv' or 'i'");
             Console.WriteLine("Lists all current pieces in inventory");
-            Console.WriteLine(ThinLine);
+            ConsoleHelper.WriteThinLine();
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("'add' or 'a'");
             Console.WriteLine("Add a new size to inventory");
-            Console.WriteLine("Example: add 19.53");
-            Console.WriteLine(ThinLine);
+            Console.WriteLine("Example: add 19.53 f/m");
+            ConsoleHelper.WriteThinLine();
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("'del' or 'd'");
             Console.WriteLine("Delete a size from inventory");
             Console.WriteLine("Example: del 19.53");
-            Console.WriteLine(ThinLine);
+            ConsoleHelper.WriteThinLine();
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("'Clear' or 'c'");
             Console.WriteLine("Clears the console");
-            Console.WriteLine(ThickLine);
-
+            ConsoleHelper.WriteThickLine();
+            Console.ResetColor();
         }
     }
 }
