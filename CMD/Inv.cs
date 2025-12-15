0488fae79aa9ffeb3b459034b73a990bc98fb10a
cheave2 <cheave2@wgu.edu>
2025-12-14 22:25:25 -0600

    Reformatted output of Inventory command

--- CMD/Inv.cs
+++ CMD/Inv.cs
@@ -9,9 +9,24 @@ namespace Extrusion_Calculator.CMD
 {
     public class Inv
     {
-        public static void InvCommand(SortedSet<double> inv)
+        public static void InvCommand(SortedSet<InventoryPiece> inv)
         {
-            Console.WriteLine("Inventory: " + string.Join(", ", inv.Select(x => $"{x}\"")));
+            if (inv.Count == 0)
+                Console.WriteLine("Inventory: (empty)");
+            else
+            {
+                ConsoleHelper.WriteThickLine();
+                Console.WriteLine("Inventory:");
+                ConsoleHelper.WriteThinLine();
+                for (int i = 0; i < inv.Count; i++)
+                {
+                    Console.WriteLine(
+                        $"{inv.ElementAt(i).Length}\" " +
+                        $"{char.ToUpperInvariant(inv.ElementAt(i).End1)}/" +
+                        $"{char.ToUpperInvariant(inv.ElementAt(i).End2)}");
+                }
+                ConsoleHelper.WriteThickLine();
+            }
         }
     }
 }
